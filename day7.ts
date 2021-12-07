bea4eb5ce938ef32f8b8cd6f383c2ab0746c041b
Shawn Tabai <s.tabai@commure.com>
2021-12-06 21:26:13 -0800

    feat: day 7 exercise

---
+++ day7.ts
@@ -0,0 +1,58 @@
+import { allInputTokenParser, solve } from "./helpers.ts";
+
+await solve({
+  baseFileName: 'day7',
+
+  parser: allInputTokenParser({
+    parseFn: Number,
+  }),
+
+  expectedTestResults: {
+    part1: 37,
+    part2: 168,
+  },
+
+  part1: inputs => {
+    const min = Math.min(...inputs.parsed);
+    const max = Math.max(...inputs.parsed);
+    let lowestCost: number|undefined;
+    for (let i = min; i <= max; i++) {
+      const cost = calculateFuelCostConstant(inputs.parsed, i);
+      if (lowestCost == null || cost < lowestCost) {
+        lowestCost = cost;
+      }
+    }
+    return lowestCost;
+  },
+
+  part2: inputs => {
+    const min = Math.min(...inputs.parsed);
+    const max = Math.max(...inputs.parsed);
+    let lowestCost: number|undefined;
+    for (let i = min; i <= max; i++) {
+      const cost = calculateFuelCostIncreasing(inputs.parsed, i);
+      if (lowestCost == null || cost < lowestCost) {
+        lowestCost = cost;
+      }
+    }
+    return lowestCost;
+  },
+});
+
+function calculateFuelCostConstant(positions: number[], alignedPosition: number) {
+  let cost = 0;
+  positions.forEach(n => cost += Math.abs(alignedPosition - n));
+  return cost;
+}
+function calculateFuelCostIncreasing(positions: number[], alignedPosition: number) {
+  let cost = 0;
+  positions.forEach(n => cost += triangular(Math.abs(alignedPosition - n)));
+  return cost;
+}
+function triangular(num: number) {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
